cb40c030a66bb620502113429b2a80e113f601e3
Matt Batman <batman.matt@gmail.com>
2016-12-04 23:13:46 -0500

    Started laying out data structure, adjust text size for smaller screens

---
+++ js/data.js
@@ -0,0 +1,8 @@
+export const Data = [
+	{
+		quote: 'You ever tried skiing on mushrooms?',
+		quotee: 'Aubrey Marcus',
+		platform: 'Joe Rogan Experience, #878'
+		link: 'https://www.youtube.com/watch?v=2xplJOx3Vjw&t=2h27m49s'
+	}
+];
