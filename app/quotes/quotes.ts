e6fa764632e2308ba40faaf01f478e40010b5789
Matt Batman <batman.matt@gmail.com>
2017-01-06 08:29:33 -0500

    Made string quotes templates to feed into blockquote innerHTML property binding, added a quote

--- app/quotes/quotes.ts
+++ app/quotes/quotes.ts
@@ -1,17 +1,21 @@
 export const QUOTES = [
   {
-    quote: 'You ever tried skiing on mushrooms?',
+    quote: '<p>You ever tried skiing on mushrooms?</p>',
     quotee: 'Aubrey Marcus',
     platform: 'Joe Rogan Experience, #878',
     link: 'https://www.youtube.com/watch?v=2xplJOx3Vjw&t=2h27m49s',
     length: 'short'
   },
   {
-    quote: `You’ve probably heard the fable about, I think it’s Buridan’s donkey,
-    who…it’s a fable about a donkey that is standing halfway in-between a pile
-    of hay and a bucket of water. And he just keeps looking left, to the hay, or
-    right, to the water, trying to decide. Hay, or water? Hay, or water? And he
-    is unable to decide, so he eventually falls over and dies of both hunger and thirst.`,
+    quote: `
+    <p>
+      You’ve probably heard the fable about, I think it’s Buridan’s donkey,
+      who…it’s a fable about a donkey that is standing halfway in-between a pile
+      of hay and a bucket of water. And he just keeps looking left, to the hay, or
+      right, to the water, trying to decide. Hay, or water? Hay, or water? And he
+      is unable to decide, so he eventually falls over and dies of both hunger and thirst.
+    </p>
+    `,
     quotee: 'Derek Sivers',
     platform: 'The Tim Ferriss Show',
     link: 'http://fourhourworkweek.com/2016/11/21/tools-of-titans-derek-sivers-distilled/',
@@ -19,26 +23,29 @@ export const QUOTES = [
   },
   {
     quote: `
-    Dear God,
-    Thank you so much for letting us exist. I can't believe we got even a few moments
-    to experience what it's like to be thrust into time as monkey-descendants
-    with an advanced biocomputer that allows us to experience a past, present, and
-    future. And though, at this moment, we may find ourselves surrounded by deeply
-    brainwashed weirdos whose entire conceptualization of reality has been constructed
-    for them by religious institutions, corporations, or politicians, please help
-    us remember that our family is actually me, wrapped around the infinite
-    goodness of the universe, and just because we're currently acting like vicious
-    hyenas doesn't mean that we don't suffer, just like anyone else. Help us
-    see beyond our temporary mask of cuntiness, to the never ending transcendent glory
-    that our puffy, wrinkled, alcoholic, racist, intolerant, hypocritical, news-obsessed
-    skin-suits hang upon. And thank you for this incredible show called human
-    incarnation. Please grant everyone at this table a lifetime of ever increasing,
-    powerful orgasms. Orgasms that move all of us in the direction of heightened
-    states of consciousness, so that we can truly allow ourselves to surrender
-    to the strange attractor at the end of time, that is rapidly transforming
-    history into something that will be completely unrecognizable to those that
-    survive the economic and societal upheavals that await for us as we go plunging
-    into the singularity. Thank you God, Hare Krishna.
+    <p>Dear God,</p>
+    <p>
+      Thank you so much for letting us exist. I can't believe we got even a few moments
+      to experience what it's like to be thrust into time as monkey-descendants
+      with an advanced biocomputer that allows us to experience a past, present, and
+      future. And though, at this moment, we may find ourselves surrounded by deeply
+      brainwashed weirdos whose entire conceptualization of reality has been constructed
+      for them by religious institutions, corporations, or politicians, please help
+      us remember that our family is actually me, wrapped around the infinite
+      goodness of the universe, and just because we're currently acting like vicious
+      hyenas doesn't mean that we don't suffer, just like anyone else. Help us
+      see beyond our temporary mask of cuntiness, to the never ending transcendent glory
+      that our puffy, wrinkled, alcoholic, racist, intolerant, hypocritical, news-obsessed
+      skin-suits hang upon. And thank you for this incredible show called human
+      incarnation. Please grant everyone at this table a lifetime of ever increasing,
+      powerful orgasms. Orgasms that move all of us in the direction of heightened
+      states of consciousness, so that we can truly allow ourselves to surrender
+      to the strange attractor at the end of time, that is rapidly transforming
+      history into something that will be completely unrecognizable to those that
+      survive the economic and societal upheavals that await for us as we go plunging
+      into the singularity.
+    </p>
+    <p>Thank you God, Hare Krishna</p>
     `,
     quotee: 'Duncan Trussell',
     platform: 'The Duncan Trussell Family Hour',
@@ -46,10 +53,41 @@ export const QUOTES = [
     length: 'long'
   },
   {
-    quote: `8 hours? There's 24 hours in a day, bro. What are you worried about?`,
+    quote: `
+    <p>
+      8 hours? There's 24 hours in a day, bro. What are you worried about?
+    </p>`,
     quotee: 'Joe Rogan',
     platform: 'Joe Rogan Experience, #895',
     link: 'https://www.youtube.com/watch?v=5r-vP3N2uAg&t=51m40s',
     length: 'short'
+  },
+  {
+    quote: `
+    <p>
+      Before I had children, I used to think of people as being sort of static.
+      Obviously I was younger, and when you’re...especially when you’re really
+      young, when you're like 20 or something like that, you don’t really take
+      into consideration that the people around you used to be your age. You
+      kind of know it in an abstract sort of a sense, but once you actually have
+      a baby, and then like, 5 years later the baby’s talking to you and you’re
+      having conversations you’re like, “Ah, you’re fucking learning shit now.”
+      And then 10 years later the baby’s in high school and you’re like, “Oh
+      Jesus Christ you’re almost a man.” And then…well, you realize, “Oh, we’re
+      all babies who had babies.” And then you raise those babies and then they
+      become adults and have babies of their own, but there are no grown ups.
+      It’s bullshit. It doesn’t exist. Like when you’re a kid and you’re sad and
+      you go, “Oh, one day I’m going to be a grown up and this is all going to
+      make sense.” But that day never comes. You get older, but you’re a baby
+      still. You’re just an old baby. You’re an old baby with a car and fucking
+      credit card debt. And then you have a baby and then that baby grows up,
+      “Well I’m smarter than my dad. He’s a fucking idiot. My dad didn’t know
+      shit. He didn’t even have the internet."
+    </p>
+    `,
+    quotee: 'Joe Rogan',
+    platform: 'Joe Rogan Experience, #891',
+    link: 'https://www.youtube.com/watch?v=g5AKVbx2M38&t=1h24m50s',
+    length: 'long'
   }
 ];
