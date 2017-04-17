fc29f564d2693c208704596d72a1a1106241aa25
Matt Batman <batman.matt@gmail.com>
2017-04-17 19:05:49 -0400

    Added Marcus Aurelius quote

--- app/quotes/quotes.ts
+++ app/quotes/quotes.ts
@@ -450,5 +450,18 @@ export const QUOTES = [
     platform: 'Joe Rogan on Happiness',
     link: 'https://www.youtube.com/watch?v=bMlsPTH62Ks',
     length: 'long'
+  },
+  {
+    quote: `
+    <p>
+      From Rusticus...I learned to read carefully and not be satisfied with a
+      rough understanding of the whole, and not to agree too quickly with those
+      who have a lot to say about something.
+    </p>
+    `,
+    quotee: 'Marcus Aurelius',
+    platform: 'Meditations, 1.7.3 via The Daily Stoic',
+    link: 'https://www.amazon.com/Daily-Stoic-Meditations-Wisdom-Perseverance/dp/0735211736?tag=ryanholnet-20',
+    length: 'short'
   }
 ];
