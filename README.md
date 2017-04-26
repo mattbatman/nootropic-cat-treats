c503308dc9dc909063b287c2694f0649079c4610
Matt Batman <batman.matt@gmail.com>
2017-04-26 16:23:40 -0400

    Upgraded to Angular 4, migrated to Angular CLI

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Nootropic Cat Treats
 Nootropic Cat Treats is a random quote generator, built with unnecessary
-complexity, so I can gain familiarity with @ngrx/store.
+complexity, so I can test features of Angular and @ngrx/store.
--- app/store.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-import { StoreModule } from '@ngrx/store';
-import { quote } from './reducers';
-
-@NgModule({
-  imports: [StoreModule.provideStore(quote)]
-})
-
-export class AppStoreModule {}
