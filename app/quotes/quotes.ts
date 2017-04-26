95e00c51a5002495842e2bf1ac1cf4a0c8dd6021
Matt Batman <batman.matt@gmail.com>
2017-04-25 23:21:27 -0400

    Added Epictetus quote

--- app/quotes/quotes.ts
+++ app/quotes/quotes.ts
@@ -506,5 +506,18 @@ export const QUOTES = [
     platform: 'Meditations, 2.5 via The Daily Stoic',
     link: 'https://www.amazon.com/Daily-Stoic-Meditations-Wisdom-Perseverance/dp/0735211736?tag=ryanholnet-20',
     length: 'long'
+  },
+  {
+    quote: `
+    <p>
+      If you wish to improve, be content to appear clueless or stupid in
+      extraneous matters &mdash; don't wish to seem knowledgeable. And if some
+      regard you as important, distrust yourself.
+    </p>
+    `,
+    quotee: 'Epictetus',
+    platform: 'Enchiridion, 13a via The Daily Stoic',
+    link: 'https://www.amazon.com/Daily-Stoic-Meditations-Wisdom-Perseverance-ebook/dp/B01HNJIJB2/ref=sr_1_1?ie=UTF8&qid=1493176790&sr=8-1&keywords=the+daily+stoic',
+    length: 'medium'
   }
 ];
