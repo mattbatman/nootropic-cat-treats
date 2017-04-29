428345fa730db06e40f06b009061b01d521a2e8e
Matt Batman <batman.matt@gmail.com>
2017-04-29 09:08:39 -0400

    Added epictetus quote

--- src/app/models/quotes.ts
+++ src/app/models/quotes.ts
@@ -533,5 +533,18 @@ export const QUOTES = [
     platform: 'Meditations, 2.2 via The Daily Stoic',
     link: 'https://www.amazon.com/Daily-Stoic-Meditations-Wisdom-Perseverance-ebook/dp/B01HNJIJB2/ref=sr_1_1?ie=UTF8&qid=1493176790&sr=8-1&keywords=the+daily+stoic',
     length: 'medium'
+  },
+  {
+    quote: `
+    <p>
+      When I see an anxious person, I ask myself, what do they want? For if a
+      person wasn't wanting something outside of their own control, why would
+      they be stricken by anxiety?
+    </p>
+    `,
+    quotee: 'Epictetus',
+    platform: 'Discourses, 2..13.1 via The Daily Stoic',
+    link: 'https://www.amazon.com/Daily-Stoic-Meditations-Wisdom-Perseverance-ebook/dp/B01HNJIJB2/ref=sr_1_1?ie=UTF8&qid=1493176790&sr=8-1&keywords=the+daily+stoic',
+    length: 'short'
   }
 ];
