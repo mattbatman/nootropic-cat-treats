5f573845812b156c851df8839eee31e16cccd5e1
Matt Batman <batman.matt@gmail.com>
2017-04-29 08:46:25 -0400

    Added marcus aurelius quote

--- src/app/models/quotes.ts
+++ src/app/models/quotes.ts
@@ -519,5 +519,19 @@ export const QUOTES = [
     platform: 'Enchiridion, 13a via The Daily Stoic',
     link: 'https://www.amazon.com/Daily-Stoic-Meditations-Wisdom-Perseverance-ebook/dp/B01HNJIJB2/ref=sr_1_1?ie=UTF8&qid=1493176790&sr=8-1&keywords=the+daily+stoic',
     length: 'medium'
+  },
+  {
+    quote: `
+    <p>
+      Frame your thoughts like this &mdash; you are an old person, you won't let
+      yourself be enslaved by this any longer, no longer pulled like a puppet
+      by every impulse, and you'll stop complaining about your present fortune
+      or dreading the future.
+    </p>
+    `,
+    quotee: 'Marcus Auerelius',
+    platform: 'Meditations, 2.2 via The Daily Stoic',
+    link: 'https://www.amazon.com/Daily-Stoic-Meditations-Wisdom-Perseverance-ebook/dp/B01HNJIJB2/ref=sr_1_1?ie=UTF8&qid=1493176790&sr=8-1&keywords=the+daily+stoic',
+    length: 'medium'
   }
 ];
