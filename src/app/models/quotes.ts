e4524892d3bb35d2035cdef24c5bd9f9561d3ffb
Matt Batman <batman.matt@gmail.com>
2017-04-30 10:40:29 -0400

    Added another epictetus quote

--- src/app/models/quotes.ts
+++ src/app/models/quotes.ts
@@ -543,7 +543,19 @@ export const QUOTES = [
     </p>
     `,
     quotee: 'Epictetus',
-    platform: 'Discourses, 2..13.1 via The Daily Stoic',
+    platform: 'Discourses, 2.13.1 via The Daily Stoic',
+    link: 'https://www.amazon.com/Daily-Stoic-Meditations-Wisdom-Perseverance-ebook/dp/B01HNJIJB2/ref=sr_1_1?ie=UTF8&qid=1493176790&sr=8-1&keywords=the+daily+stoic',
+    length: 'short'
+  },
+  {
+    quote: `
+    <p>
+      Who then is invincible? The one who cannot be upset by anything outside
+      their reasoned choice.
+    </p>
+    `,
+    quotee: 'Epictetus',
+    platform: 'Discourses, 1.18.21 via The Daily Stoic',
     link: 'https://www.amazon.com/Daily-Stoic-Meditations-Wisdom-Perseverance-ebook/dp/B01HNJIJB2/ref=sr_1_1?ie=UTF8&qid=1493176790&sr=8-1&keywords=the+daily+stoic',
     length: 'short'
   }
