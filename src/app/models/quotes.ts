e0b6323a1ae0c68624d3f14f09e69a8a3817e33b
Matt Batman <batman.matt@gmail.com>
2017-05-07 22:41:16 -0400

    Added Cody Wilson quote

--- src/app/models/quotes.ts
+++ src/app/models/quotes.ts
@@ -586,5 +586,16 @@ export const QUOTES = [
     platform: 'Top 8 developer Habits: Perseverance',
     link: 'https://www.youtube.com/watch?v=DwQ7psiU23I&t=10m50s',
     length: 'long'
+  },
+  {
+    quote: `
+    <p>
+      That's the thing about national conversations...not everyone gets to talk.
+    </p>
+    `,
+    quotee: 'Cody Wilson',
+    platform: 'Come and Take It',
+    link: 'https://www.amazon.com/Come-Take-Printers-Guide-Thinking-ebook/dp/B01CO34MBI/ref=sr_1_4?ie=UTF8&qid=1494211026&sr=8-4&keywords=come+and+take+it',
+    length: 'short'
   }
 ];
