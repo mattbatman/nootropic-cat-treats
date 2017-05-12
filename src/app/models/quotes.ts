6f884bb3b990d99a061a95448a28d3601706b9b9
Matt Batman <batman.matt@gmail.com>
2017-05-12 08:50:49 -0400

    Added epictetus quote

--- src/app/models/quotes.ts
+++ src/app/models/quotes.ts
@@ -597,5 +597,24 @@ export const QUOTES = [
     platform: 'Come and Take It',
     link: 'https://www.amazon.com/Come-Take-Printers-Guide-Thinking-ebook/dp/B01CO34MBI/ref=sr_1_4?ie=UTF8&qid=1494211026&sr=8-4&keywords=come+and+take+it',
     length: 'short'
+  },
+  {
+    quote: `
+    <p>
+      Whenever you get an impression of some pleasure, as with any impression,
+      guard yourself from being carried away by it, let it await your action,
+      give yourself a pause. After that, bring to mind both times, first when
+      you you have enjoyed the pleasure and later when you will regret it and
+      hate yourself. Then compare to those the joy and satisfaction you'd feel
+      for abstaining altogether. However, if a seemingly appropriate time arises
+      to act on it, don't be overcome by its comfort, pleasantness, and allure
+      &mdash; but against all of this, how much better the consciousness of
+      conquering it.
+    </p>
+    `,
+    quotee: 'Epictetus',
+    platform: 'Enchiridion, 34 via The Daily Stoic',
+    link: 'https://www.amazon.com/Daily-Stoic-Meditations-Wisdom-Perseverance-ebook/dp/B01HNJIJB2/ref=sr_1_1?ie=UTF8&qid=1493176790&sr=8-1&keywords=the+daily+stoic',
+    length: 'long'
   }
 ];
