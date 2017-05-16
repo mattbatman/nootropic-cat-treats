33bf46ff51a6b6239cd51709c167aacabac7c30e
Matt Batman <batman.matt@gmail.com>
2017-05-16 08:30:53 -0400

    Added epictetus quote

--- src/app/models/quotes.ts
+++ src/app/models/quotes.ts
@@ -627,5 +627,17 @@ export const QUOTES = [
     platform: 'Meditations, 6.26 via The Daily Stoic',
     link: 'https://www.amazon.com/Daily-Stoic-Meditations-Wisdom-Perseverance-ebook/dp/B01HNJIJB2/ref=sr_1_1?ie=UTF8&qid=1493176790&sr=8-1&keywords=the+daily+stoic',
     length: 'medium'
+  },
+  {
+    quote: `
+    <p>
+      It is quite impossible to unite happiness with a yearning for what we
+      don't have.
+    </p>
+    `,
+    quotee: 'Epictetus',
+    platform: 'Discourses, 3.24.17 via The Daily Stoic',
+    link: 'https://www.amazon.com/Daily-Stoic-Meditations-Wisdom-Perseverance-ebook/dp/B01HNJIJB2/ref=sr_1_1?ie=UTF8&qid=1493176790&sr=8-1&keywords=the+daily+stoic',
+    length: 'short'
   }
 ];
