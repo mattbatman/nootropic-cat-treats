46aee94e5457ece1e8949ccb1ca85fe75fb8fa8e
Matt Batman <batman.matt@gmail.com>
2017-05-19 08:26:01 -0400

    Added banquet epictetus quote

--- src/app/models/quotes.ts
+++ src/app/models/quotes.ts
@@ -639,5 +639,22 @@ export const QUOTES = [
     platform: 'Discourses, 3.24.17 via The Daily Stoic',
     link: 'https://www.amazon.com/Daily-Stoic-Meditations-Wisdom-Perseverance-ebook/dp/B01HNJIJB2/ref=sr_1_1?ie=UTF8&qid=1493176790&sr=8-1&keywords=the+daily+stoic',
     length: 'short'
+  },
+  {
+    quote: `
+    <p>
+      Remember to conduct yourself in life as if at a banquet.
+      As something being passed around comes to you, reach out
+      your hand and take a moderate helping. Does it pass you by?
+      Don't stop it. It hasn't yet come? Don't burn in desire for
+      it, but wait until it arrives in front of you. Act this way
+      with children, a spouse, toward position, with wealth &mdash;
+      one day it will make you worthy of a banquet with the gods.
+    </p>
+    `,
+    quotee: 'Epictetus',
+    platform: 'Enchriridion, 15 via The Daily Stoic',
+    link: 'https://www.amazon.com/Daily-Stoic-Meditations-Wisdom-Perseverance-ebook/dp/B01HNJIJB2/ref=sr_1_1?ie=UTF8&qid=1493176790&sr=8-1&keywords=the+daily+stoic',
+    length: 'medium'
   }
 ];
