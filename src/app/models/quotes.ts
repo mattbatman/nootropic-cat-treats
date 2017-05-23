d5364048fb24dda5dda310c3d3f5e35a06089337
Matt Batman <batman.matt@gmail.com>
2017-05-23 09:50:40 -0400

    Added marcus aurelius and epictetus quotes

--- src/app/models/quotes.ts
+++ src/app/models/quotes.ts
@@ -656,5 +656,33 @@ export const QUOTES = [
     platform: 'Enchriridion, 15 via The Daily Stoic',
     link: 'https://www.amazon.com/Daily-Stoic-Meditations-Wisdom-Perseverance-ebook/dp/B01HNJIJB2/ref=sr_1_1?ie=UTF8&qid=1493176790&sr=8-1&keywords=the+daily+stoic',
     length: 'medium'
+  },
+  {
+    quote: `
+    <p>
+      You shouldn't give circumstances the power to rouse anger, for they don't
+      care at all.
+    </p>
+    `,
+    quotee: 'Marcus Aurelius',
+    platform: 'Meditations, 7.38 via The Daily Stoic',
+    link: 'https://www.amazon.com/Daily-Stoic-Meditations-Wisdom-Perseverance-ebook/dp/B01HNJIJB2/ref=sr_1_1?ie=UTF8&qid=1493176790&sr=8-1&keywords=the+daily+stoic',
+    length: 'short'
+  },
+  {
+    quote: `
+    <p>
+      Keep in mind that it isn't the one who has it in for you and takes a
+      swipe that harms you, but rather the harm comes from your own belief
+      about the abuse. So when someone arouses your anger, know that it's really
+      your own opinion fueling it. Instead, make it your first response not to
+      be carried away by such impressions, for with time and distance
+      self-mastery is more easily achieved.
+    </p>
+    `,
+    quotee: 'Epictetus',
+    platform: 'Enchiridion, 20 via The Daily Stoic',
+    link: 'https://www.amazon.com/Daily-Stoic-Meditations-Wisdom-Perseverance-ebook/dp/B01HNJIJB2/ref=sr_1_1?ie=UTF8&qid=1493176790&sr=8-1&keywords=the+daily+stoic',
+    length: 'medium'
   }
 ];
