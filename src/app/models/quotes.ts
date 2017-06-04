802fe5a2601ebdc4c50801cb2eee67a9fd91be1e
Matt Batman <batman.matt@gmail.com>
2017-06-04 09:50:22 -0400

    Added daily stoic author quote

--- src/app/models/quotes.ts
+++ src/app/models/quotes.ts
@@ -684,5 +684,16 @@ export const QUOTES = [
     platform: 'Enchiridion, 20 via The Daily Stoic',
     link: 'https://www.amazon.com/Daily-Stoic-Meditations-Wisdom-Perseverance-ebook/dp/B01HNJIJB2/ref=sr_1_1?ie=UTF8&qid=1493176790&sr=8-1&keywords=the+daily+stoic',
     length: 'medium'
+  },
+  {
+    quote: `
+    <p>
+      Listen and connect with people, don't perform for them.
+    </p>
+    `,
+    quotee: 'Ryan Holiday and Stephen Hanselman',
+    platform: 'Enchiridion, 20 via The Daily Stoic',
+    link: 'https://www.amazon.com/Daily-Stoic-Meditations-Wisdom-Perseverance-ebook/dp/B01HNJIJB2/ref=sr_1_1?ie=UTF8&qid=1493176790&sr=8-1&keywords=the+daily+stoic',
+    length: 'Short'
   }
 ];
