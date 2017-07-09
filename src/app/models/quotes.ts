e7754c2bf443615aa989213f609468ffceced94b
Matt Batman <batman.matt@gmail.com>
2017-07-09 08:26:42 -0400

    Epictetus quote added

--- src/app/models/quotes.ts
+++ src/app/models/quotes.ts
@@ -738,5 +738,17 @@ export const QUOTES = [
     platform: 'The Daily Stoic',
     link: 'https://www.amazon.com/Daily-Stoic-Meditations-Wisdom-Perseverance-ebook/dp/B01HNJIJB2/ref=sr_1_1?ie=UTF8&qid=1493176790&sr=8-1&keywords=the+daily+stoic',
     length: 'short'
+  },
+  {
+    quote: `
+    <p>
+      It isn't events themselves that disturb people, but only their judgments
+      about them.
+    </p>
+    `,
+    quotee: 'Epictetus, Enchiridion, 5',
+    platform: 'The Daily Stoic',
+    link: 'https://www.amazon.com/Daily-Stoic-Meditations-Wisdom-Perseverance-ebook/dp/B01HNJIJB2/ref=sr_1_1?ie=UTF8&qid=1493176790&sr=8-1&keywords=the+daily+stoic',
+    length: 'short'
   }
 ];
