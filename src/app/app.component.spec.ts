c4d6fa51c807ea2cb4ba75dd00f7ecc4954c6c24
Matt Batman <batman.matt@gmail.com>
2017-07-26 09:04:17 -0400

    Set up a mock store

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -7,23 +7,27 @@ import { Subject } from 'rxjs/Subject';
 
 import { AppComponent } from './app.component';
 import * as fromRoot from './reducers';
+import { MockStore } from '../testing-helpers/store';
 
 describe('AppComponent', () => {
   let comp: AppComponent;
   let fixture: ComponentFixture<AppComponent>;
   let de: DebugElement;
   let el: HTMLElement;
-  let _store: Store<fromRoot.State>;
+  let mockedStore: Store<fromRoot.State>;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [ AppComponent ],
-      imports: [StoreModule.provideStore(fromRoot.reducer)]
+      imports: [StoreModule.provideStore(fromRoot.reducer)],
+      providers: [
+        { provide: Store, useValue: new MockStore({ quoteNumber: 0 }) }
+      ]
     });
 
     fixture = TestBed.createComponent(AppComponent);
 
-    _store = fixture.debugElement.injector.get(Store);
+    mockedStore = fixture.debugElement.injector.get(Store);
 
     comp = fixture.componentInstance;
 
@@ -36,30 +40,3 @@ describe('AppComponent', () => {
     expect(el.textContent).toContain('New Quote');
   });
 });
-
-// export function mockStore<T>({
-//   actions = new Subject<Action>(),
-//   states = new Subject<T>()
-// }: {
-//   actions?: Subject<Action>,
-//   states?: Subject<T>
-// }): Store<T> {
-//
-//   const result = states as any;
-//   result.dispatch = (action: Action) => actions.next(action);
-//   return result;
-// }
-
-/*
-NOTE
-Here are two ways to test a component that uses the store
-1. Inject the actual store
-https://blog.realworldfullstack.io/real-world-angular-part-9-unit-testing-c62ba20b1d93
-
-2. Mock the store
-https://stackoverflow.com/questions/43215024/how-to-mock-ngrx-store
-or later in the article above
-
-It seems to be considered far better to mock the store, but injecting the real
-ngrx store is initially easier
-*/
