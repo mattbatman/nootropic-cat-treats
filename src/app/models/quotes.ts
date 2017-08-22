71e18345406cbe7dcec84846120417d203cd8299
Matt Batman <batman.matt@gmail.com>
2017-08-22 08:18:44 -0400

    Added steven pressfield quote

--- src/app/models/quotes.ts
+++ src/app/models/quotes.ts
@@ -863,5 +863,17 @@ export const QUOTES: Array<Quote> = [
     platform: 'Jocko Podcast',
     link: 'https://www.youtube.com/watch?v=IdTMDpizis8',
     length: 'long'
+  },
+  {
+    quote: `
+    <p>
+      We don't tell ourselves, "I'm never going to write my symphony." Instead
+      we say, "I'm going to write my symphony; I'm just going to start tomorrow."
+    </p>
+    `,
+    quotee: 'Steven Pressfield',
+    platform: 'The War of Art',
+    link: 'https://www.amazon.com/War-Art-Through-Creative-Battles/dp/1936891026/ref=sr_1_1?ie=UTF8&qid=1503404221&sr=8-1&keywords=the+war+of+art',
+    length: 'short'
   }
 ];
