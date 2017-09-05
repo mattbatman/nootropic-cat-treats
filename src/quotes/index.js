df71ebe7873958be9f3a5ab23a79c2fbb4b570e1
Matt Batman <batman.matt@gmail.com>
2017-09-05 07:53:19 -0400

    Added epictetus quote

--- src/quotes/index.js
+++ src/quotes/index.js
@@ -867,6 +867,20 @@ const quotes = [
     platform: 'The War of Art',
     link: 'https://www.amazon.com/War-Art-Through-Creative-Battles/dp/1936891026/ref=sr_1_1?ie=UTF8&qid=1503404221&sr=8-1&keywords=the+war+of+art',
     length: 'short'
+  },
+  {
+    quote: `
+    <p>
+      We cry to God Almighty, how can we escape this agony? Fool, don’t you have
+      hands? Or could it be God forgot to give you a pair? Sit and pray your
+      nose doesn’t run! Or, rather just wipe your nose and stop seeking a
+      scapegoat.
+    </p>
+    `,
+    quotee: 'Epictetus',
+    platform: 'Discourses, 2.16.13',
+    link: 'https://www.amazon.com/Daily-Stoic-Meditations-Wisdom-Perseverance-ebook/dp/B01HNJIJB2/ref=sr_1_1?ie=UTF8&qid=1493176790&sr=8-1&keywords=the+daily+stoic',
+    length: 'medium'
   }
 ];
 
