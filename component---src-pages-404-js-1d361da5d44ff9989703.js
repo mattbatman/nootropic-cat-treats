7edce8151b5b780f70cf4d18e88dca0ab5c90c55
Matt Batman <batman.matt@gmail.com>
2021-11-30 16:11:58 -0500

    Updates

--- component---src-pages-404-js-1d361da5d44ff9989703.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunknootropic_cat_treats=self.webpackChunknootropic_cat_treats||[]).push([[883],{9616:function(e,t,a){a.r(t);var n=a(7294),c=a(5444);t.default=function(){return n.createElement("div",{className:"App"},n.createElement("div",{className:"container"},n.createElement("p",{className:"jumbo"},"This page was not found. Try starting ",n.createElement(c.Link,{to:"/quotes/1"},"from the beginning."))))}}}]);
-//# sourceMappingURL=component---src-pages-404-js-1d361da5d44ff9989703.js.map
