df0e32a4560a5500e3d2238ac995e952989b8f96
Matt Batman <batman.matt@gmail.com>
2022-11-26 17:02:16 -0500

    Load and change font

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,5 +27,18 @@ module.exports = {
         icon: "src/images/favicon.png",
       },
     },
+    {
+      resolve: `gatsby-omni-font-loader`,
+      options: {
+        enableListener: true,
+        preconnect: [`https://fonts.googleapis.com`, `https://fonts.gstatic.com`],
+        web: [
+          {
+            name: 'DM Sans',
+            file: 'https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;700&display=swap',
+          },
+        ],
+      },
+    },
   ],
 };
