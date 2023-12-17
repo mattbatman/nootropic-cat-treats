38d5794400f32e6578ec8070351a0553020257e7
Matt Batman <batman.matt@gmail.com>
2023-12-17 11:28:54 -0500

    Allow navigation forward or backward

--- src/components/NavBarLink/index.tsx
+++ src/components/NavBarLink/index.tsx
@@ -1,10 +1,23 @@
 import React from 'react';
 import { getIsAtEnd } from '../../utility';
+import type { CollectionEntry } from 'astro:content';
 
 interface NavBarLinkProps {
   backwardOrForward: 'backward' | 'forward';
   id: number;
-  playlist: any[];
+  playlist: CollectionEntry<'quotes'>[];
+}
+
+function getNextLocation({
+  id,
+  backwardOrForward
+}: {
+  id: number;
+  backwardOrForward: 'backward' | 'forward';
+}) {
+  const nextId = backwardOrForward === 'forward' ? id + 1 : id - 1;
+
+  return `/quotes/${nextId}`;
 }
 
 const NavBarLink: React.FC<NavBarLinkProps> = ({
@@ -13,12 +26,13 @@ const NavBarLink: React.FC<NavBarLinkProps> = ({
   playlist
 }) => {
   const isAtEnd = getIsAtEnd({ id, playlist, backwardOrForward });
+  const nextLocation = getNextLocation({ id, backwardOrForward });
 
   if (!playlist || playlist.length === 0 || isAtEnd) {
     return <span className={`${backwardOrForward} disabled`} />;
   }
 
-  return <a className={backwardOrForward} />;
+  return <a className={backwardOrForward} href={nextLocation} />;
 };
 
 export { NavBarLink };
