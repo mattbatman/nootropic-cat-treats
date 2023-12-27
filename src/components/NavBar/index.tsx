2de4c17dedb48a4467edb54ef76c4b37f31c6f66
Matt Batman <batman.matt@gmail.com>
2023-12-27 18:18:32 -0500

    Update title bar displayed based on query param

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -1,17 +1,28 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { NavBarLink } from '../NavBarLink';
+import { useStore } from '@nanostores/react';
+import { $quotee, updateQuotee } from '../stores/quotee';
 import type { CollectionEntry } from 'astro:content';
 
 const NavBar: React.FC<{
   id: number;
   playlist: CollectionEntry<'quotes'>[];
 }> = ({ id, playlist }) => {
+  const quotee$ = useStore($quotee);
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(window.location.search);
+    const quoteeInParam = urlParams.get('quotee');
+
+    updateQuotee(quoteeInParam);
+  }, []);
+
   return (
     <nav className="closed">
       <NavBarLink backwardOrForward="backward" id={id} playlist={playlist} />
       <button className="playlist-button">
         <span className="playlist-button-text">
-          <a href="/quotees">Nootropic Cat Treats</a>
+          <a href="/quotees">{quotee$ ? quotee$ : 'Nootropic Cat Treats'}</a>
         </span>
       </button>
       <NavBarLink backwardOrForward="forward" id={id} playlist={playlist} />
