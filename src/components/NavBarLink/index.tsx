bce72aba47008617e6f305d15ce5a53a0ff360e4
Matt Batman <batman.matt@gmail.com>
2023-12-28 09:26:02 -0500

    Keep next quote on playlist query param

--- src/components/NavBarLink/index.tsx
+++ src/components/NavBarLink/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useStore } from '@nanostores/react';
 import { $history } from '../stores/history';
+import { $quotee } from '../stores/quotee';
 import { getIsAtEnd, randomNoRepeat } from '../../utility';
 import type { CollectionEntry } from 'astro:content';
 
@@ -14,34 +15,49 @@ function getNextLocation({
   id,
   backwardOrForward,
   history,
-  playlist
+  playlist,
+  quotee
 }: {
   id: number;
   backwardOrForward: 'backward' | 'forward';
   history: number[];
   playlist: CollectionEntry<'quotes'>[];
+  quotee: string | null;
 }) {
   const placeInHistory = history.indexOf(id);
   let nextId = id;
 
   if (backwardOrForward === 'forward') {
-    if (placeInHistory === history.length - 1) {
+    const isAtEndOfHistory = placeInHistory === history.length - 1;
+
+    if (isAtEndOfHistory) {
+      // get a random new location to add to history
       nextId = randomNoRepeat({
         hist: history,
         cur: id,
-        min: 0,
+        min: 1,
         max: playlist.length
       });
+
+      nextId = playlist[nextId].data.id;
     } else {
+      // move forward in history
       nextId = history[placeInHistory + 1];
     }
   } else {
+    // if backward button
     if (placeInHistory !== 0) {
       nextId = history[placeInHistory - 1];
     }
   }
 
-  return `/quotes/${nextId}`;
+  const link = `/quotes/${nextId}`;
+
+  if (quotee === null) {
+    return link;
+  }
+
+  return `${link}?quotee=${quotee}`;
 }
 
 const NavBarLink: React.FC<NavBarLinkProps> = ({
@@ -50,12 +66,14 @@ const NavBarLink: React.FC<NavBarLinkProps> = ({
   playlist
 }) => {
   const history = useStore($history);
+  const quotee = useStore($quotee);
   const isAtEnd = getIsAtEnd({ id, history, playlist, backwardOrForward });
   const nextLocation = getNextLocation({
     id,
     backwardOrForward,
     history,
-    playlist
+    playlist,
+    quotee
   });
 
   if (!playlist || playlist.length === 0 || isAtEnd) {
--- src/components/stores/quotee.ts
+++ src/components/stores/quotee.ts
@@ -1,12 +1,20 @@
 import { atom } from 'nanostores';
+import { $history } from './history';
 
 const $quotee = atom<string | null>(null);
 
-function updateQuotee(newQuotee: string) {
+function updateQuotee({
+  newQuotee,
+  id
+}: {
+  newQuotee: string | null;
+  id: number;
+}) {
   const currentQuotee = $quotee.get();
 
   if (currentQuotee !== newQuotee) {
     $quotee.set(newQuotee);
+    $history.set([id]);
   }
 }
 
