0a7a0ac4df802b525e40f00030da5d830845a49d
Matt Batman <batman.matt@gmail.com>
2024-01-03 16:18:17 -0500

    Allow quotee across pages

--- src/components/stores/quotee.ts
+++ src/components/stores/quotee.ts
@@ -1,14 +1,23 @@
-import { atom } from 'nanostores';
+import { persistentAtom } from '@nanostores/persistent';
 import { $history } from './history';
 
-const $quotee = atom<string>('');
+const QUOTEE_IN_LOCAL_STORAGE = 'quotee';
+
+const $quotee = persistentAtom<string>(
+  QUOTEE_IN_LOCAL_STORAGE,
+  '', // initial value
+  {
+    encode: JSON.stringify,
+    decode: JSON.parse
+  }
+);
 
 function updateQuotee({ newQuotee, id }: { newQuotee: string; id: number }) {
   const currentQuotee = $quotee.get();
 
   if (currentQuotee !== newQuotee) {
     $quotee.set(newQuotee);
-    //    $history.set([id]);
+    $history.set([id]);
   }
 }
 
