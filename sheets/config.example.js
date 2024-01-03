75099a748868b6081c2172c42440f2c48f7c6e7d
Matt Batman <batman.matt@gmail.com>
2024-01-03 16:36:19 -0500

    Delete Gatsby files after rebase

--- sheets/config.example.js
+++
@@ -1,9 +0,0 @@
-const config = {
-  spreadsheetId: 'gibberish_from_sheet_url',
-  range: 'hello!A:F',
-  tokenPath: 'sheets/token.json',
-  credentialsPath: 'sheets/credentials.json',
-  outputPath: 'sheets/quotes.json'
-};
-
-module.exports = config;
