e4b7624ec5e0f8b6de93bda67df9e1f90b181c17
Matt Batman <batman.matt@gmail.com>
2024-09-27 19:45:57 -0400

    Change domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://nootropiccattreats.space',
+  site: 'https://nootropiccattreats.win',
   integrations: [react(), sitemap()]
 });
